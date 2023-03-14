91238ee36d4e2f73f9ef91bb89d1039921d76363
BogartDME <edwin.pietrowski@gmail.com>
2023-03-14 17:02:27 -0400

    updated the logic in database.js in client side to update and get data from the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,51 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // TODO: Add logic for a method that gets all the content from the database
+  export const getDb = async () =>{ 
 
+  //creates a connection to the db and dictates which version to use
+  const jateDb = await openDB('jate', 1);
+
+  //generates a new transaction- tells you which db and what are the data privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //opens the desired object store
+  const store = tx.objectStore('jate');
+
+  //uses the getAll method to get all data in the db
+  const request = store.getAll();
+
+  //gets confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+  
+    console.log('🚀 - data saved to the database', result);
+    console.error('putDb not implemented');
+    return result;
+  };
+
+
+  // TODO: Add logic to a method that accepts some content and adds it to the database
+  export const putDb = async (id, content) =>{ 
+    
+    //creates a connection to the db and dictates which version to use
+    const jateDb = await openDB('jate', 1);
+    
+    //generates a new transaction- tells you which db and what are the data privileges
+    const tx = jateDb.transaction('jate', 'readwrite');
+    
+    //opens the desired object store
+    const store = tx.objectStore('jate');
+    
+    //uses the put method to update data in the db
+    const request = store.put({ id: 1, jate: content });
+    
+    //gets confirmation of the request
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+    console.log('PUT to the database');
+    console.error('getDb not implemented');
+}
 initdb();
