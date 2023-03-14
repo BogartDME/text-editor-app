4202f39ae260c613d5e4b7efcce86ac17cbbee6e
BogartDME <edwin.pietrowski@gmail.com>
2023-03-14 18:12:09 -0400

    added asset cache function to src-sw.js in client side

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 
-  // TODO: Add logic for a method that gets all the content from the database
+  
   export const getDb = async () =>{ 
 
   //creates a connection to the db and dictates which version to use
@@ -38,7 +38,7 @@ const initdb = async () =>
   };
 
 
-  // TODO: Add logic to a method that accepts some content and adds it to the database
+  
   export const putDb = async (id, content) =>{ 
     
     //creates a connection to the db and dictates which version to use
